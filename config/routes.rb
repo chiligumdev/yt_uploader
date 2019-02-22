c2821d59f6996ecbb9a74568872e14c49074decc
Lean <lndr.figueredo@gmail.com>
2019-02-22 19:11:26 -0300

    Adding omniauth-google-oauth2 gem and create some new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :videos, only: %w(new index)
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: 'sessions#fail'
 
   root to: 'videos#index'
 end
