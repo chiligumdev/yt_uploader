908f2d2e2513877361a25a4151eda49e78a27324
Lean <lndr.figueredo@gmail.com>
2019-02-22 21:54:48 -0300

    Final methods in videos controller

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem 'youtube_it', github: 'bodrovis/youtube_it'
 gem "figaro"
 gem 'omniauth-google-oauth2'
 gem 'bootstrap-sass'
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :videos, only: %w(new index)
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: 'sessions#fail'
+  post '/videos/get_upload_token', to: 'videos#get_upload_token', as: :get_upload_token
+  get '/videos/get_video_uid', to: 'videos#get_video_uid', as: :get_video_uid
 
   root to: 'videos#index'
 end
