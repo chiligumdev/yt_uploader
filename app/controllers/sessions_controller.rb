84080055b602af423c4a884fd93aa2ce64341f03
Lean <lndr.figueredo@gmail.com>
2019-02-22 21:03:17 -0300

    Sessions create method and current user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,17 @@
+# sessions controller file
 class SessionsController < ApplicationController
+  def create
+    auth = request.env['omniauth.auth']
+    user = User.find_or_initialize_by(uid: auth['uid'])
+    user.token = auth['credentials']['token']
+    user.name = auth['info']['name']
+    user.save
+    sessions[:user_id] = user.id
+    flash[:success] = "Welcome #{user.name}"
+    redirect_to new_video_path
+  end
+
+  def fail
+    render text: "Sorry, but the following error has ocurred: #{params[:messge]}. Please, try again or contact administrator"
+  end
 end
