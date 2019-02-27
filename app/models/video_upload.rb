cc0d90788cdf33a07b7cc1e1af127c68f71cebd8
Lean <lndr.figueredo@gmail.com>
2019-02-27 13:08:37 -0300

    Uploading videos to youtube method

--- app/models/video_upload.rb
+++ app/models/video_upload.rb
@@ -6,4 +6,9 @@ class VideoUpload < ActiveType::Object
 
   validates :file, presence: true
   validades :title, presence: true
+
+  def upload(user)
+    account = Yt::Account.new access_token: user.token
+    account.upload_video self.file, title: self.title, description: self.description
+  end
 end
