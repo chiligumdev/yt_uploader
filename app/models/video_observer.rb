a1d1582d8c0b4bc8d2ac064b474727e87060f5c9
Lean <lndr.figueredo@gmail.com>
2019-02-28 13:24:17 -0300

    Try

--- app/models/video_observer.rb
+++ app/models/video_observer.rb
@@ -2,7 +2,7 @@
 class VideoObserver < ActiveRecord::Observer
   def before_save(resource)
     video = Yt::Video.new url: resource.link
-    resource.uid = video.uid
+    resource.uid = video.id
     resource.title = video.title
     resource.likes = video.like_count
     resource.deslikes = video.dislike_count
